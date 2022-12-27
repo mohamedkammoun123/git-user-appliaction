7e63cb86305b25b05d609ce60b63c9f081550d65
ka <mohamed.kamoun@ensi-uma.tn>
2022-12-27 11:18:36 +0100

    add_AIDL_branch

--- app/src/main/java/com/example/my_application/View/MainActivity.java
+++ app/src/main/java/com/example/my_application/View/MainActivity.java
@@ -1,26 +1,55 @@
 package com.example.my_application.View;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.my_application.AIDLService.AIDLColorService;
 import com.example.my_application.Controller.UserController;
+import com.example.my_application.IAIDLColorInterface;
 import com.example.my_application.Model.User;
 import com.example.my_application.R;
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+    IAIDLColorInterface iAidlColorService ;
+    private static final String TAG = "MainActivity";
 
+    private ServiceConnection myConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            iAidlColorService = IAIDLColorInterface.Stub.asInterface(iBinder);
+            Log.d(TAG,"Remote config Service Connected");
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         MaterialButton search_button = (MaterialButton) findViewById(R.id.search_button);
         EditText login_edit_text = (EditText) findViewById(R.id.search_bar);
         MaterialButton relation_button = (MaterialButton) findViewById(R.id.id_relation);
+
+        Intent intent = new Intent("AIDLColorService");
+        System.out.println("start to set intent package of AIDL Service");
+        intent.setPackage("com.example.my_application");
+        System.out.println("end of call set intent function ");
+        bindService(intent,myConnection,BIND_AUTO_CREATE);
+
         search_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -45,6 +74,13 @@ public class MainActivity extends AppCompatActivity {
         relation_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                try {
+                    System.out.println("try to use getColor from Serv");
+                    int color = iAidlColorService.getColor();
+                    relation_button.setBackgroundColor(color);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
                 String login = login_edit_text.getText().toString();
                 UserController userController = new UserController(login);
                 User user = userController.fromJsonToUser();
